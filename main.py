6d0da4c11444c74ad5483a3136aaea10a62c0430
Eduardo DEV <duboy42@gmail.com>
2026-05-06 09:20:39 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify, request
-
+#Criação de API jogos
+#Flask como framework
 app = Flask(__name__)
 
+Dicionario Json para criação da API e dos elementos
 jogos = [
     {
         'id': 1,
@@ -26,10 +28,12 @@ jogos = [
     }
 ]
 
+#Rota para ver todos os jogos
 @app.route('/jogos', methods=['GET'])
 def ver_jogos():
     return jsonify(jogos)
 
+#Rota para ver jogo por ID
 @app.route('/jogos/<int:id>', methods=['GET'])
 def ver_jogos_id(id):
     for jogo in jogos:
@@ -37,6 +41,7 @@ def ver_jogos_id(id):
             return jsonify(jogo)
     return jsonify({'erro': 'jogo não encontrado'}),404
 
+#Rota para editar jogo, editar suas informações
 @app.route('/jogos/<int:id>', methods=['PUT'])
 def editar_jogos(id):
     jogo_alterar = request.get_json()
@@ -44,13 +49,15 @@ def editar_jogos(id):
         if jogo.get('id') == id:
             jogos[indice].update(jogo_alterar)
             return jsonify(jogos[indice])
-        
+
+#Rota para adicionar jogo novo
 @app.route('/jogos', methods=["POST"])
 def jogo_novo():
     jogo_novo = request.get_json()
     jogos.append(jogo_novo)
     return jsonify(jogos)
-
+    
+#Rota para deletar jogo por ID
 @app.route('/jogos/<int:id>', methods=['DELETE'])
 def excluir_id(id):
     for indice,excluir in enumerate(jogos):
